895baf0eb209a84ee618ad72e8a2e9b0c3e7691c
Drake Williamson <williamson.drake@outlook.com>
2024-05-15 15:53:31 -0700

    feat: gather assets

--- webpack.common.js
+++ webpack.common.js
@@ -3,19 +3,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'head',
-        scriptLoading: 'defer'
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
