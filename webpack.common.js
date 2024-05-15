dc48bfb2bbf8612df830c31fe9acd39f1da3d110
Drake Williamson <williamson.drake@outlook.com>
2024-05-15 16:03:51 -0700

    fix: move svg

--- webpack.common.js
+++ webpack.common.js
@@ -21,16 +21,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
       },
     ],
   },
